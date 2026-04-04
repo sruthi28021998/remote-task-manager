b4bf6da88bf2174ef86cc250a0fc5d359c3433ee
sruthi28021998 <sruthibalabhadruni1998@gmail.com>
2026-04-04 13:04:42 +0530

    Added vercel config and module exports

--- backend/server.js
+++ backend/server.js
@@ -92,3 +92,5 @@ app.delete('/delete-task/:id', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
+module.exports=app;
